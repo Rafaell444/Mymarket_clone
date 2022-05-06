fc7fb57077b6d2413f5452ae7a90aaecedaa5b37
rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2022-05-06 17:32:45 +0400

    remove page done/filter by category done

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('product/<str:pk>/', views.product, name="product"),
+
     path('create_product/', views.createproduct, name="create_product"),
-    path('update_product/<str:pk>/', views.updateproduct, name="update_product")
+    path('update_product/<str:pk>/', views.updateproduct, name="update_product"),
+    path('delete_product/<str:pk>/', views.deleteproduct, name="delete_product")
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Product, Category
 from .forms import ProductForm
@@ -14,8 +15,19 @@ from .forms import ProductForm
 
 
 def home(request):
-    products = Product.objects.all()
+    q = request.GET.get("q") if request.GET.get('q') is not None else ""
+
+    products = Product.objects.filter(
+        Q(category__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     category = Category.objects.all()
+
+    for product in products:
+        product.created = product.created.replace(second=0, microsecond=0)
+        product.updated = product.updated.replace(second=0, microsecond=0)
+
     context = {"products": products, "category": category}
     return render(request, "homepage.html", context)
 
@@ -45,11 +57,21 @@ def updateproduct(request, pk):
     form = ProductForm(instance=product)
 
     if request.method == "POST":
-        form = ProductForm(request.POST,instance=product)
+        form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
             return redirect("home")
 
     context = {"form": form}
 
-    return render(request, "product_form.html",context)
+    return render(request, "product_form.html", context)
+
+
+def deleteproduct(request, pk):
+    product = Product.objects.get(id=pk)
+
+    if request.method == "POST":
+        product.delete()
+        return redirect("home")
+
+    return render(request, "delete_product.html", context={"product": product})
